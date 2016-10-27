d9b1e897255ee1062c6a91dda3b491458ba052eb
nayanambuj <nayan003.umn.edu>
2016-10-26 20:02:41 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,82 +1,31 @@
-// MongoDB connection parameters
 var ejs = require('ejs');
 var mongojs = require("mongojs");
 var db = mongojs('mongodb://webcrows:umncsfall16@ds019068.mlab.com:19068/webcrowsdb', ['account','progress']);
-//var othello = require('./client/js/othello.js');
-
-// Start the server
 var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 app.use(bodyparser());
-
 app.set('view engine', 'ejs');
-//var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
-//app.engine('handlebars', handlebars.engine);
-//app.set('view engine', 'handlebars');
 var serv = require('http').Server(app);
-;
-//app.use(express.static(__dirname + '/client'));
 
-
-function createUser(username, password, password_confirmation, callback){
-    if (password !== password_confirmation) {
-    var err = 'The passwords do not match';
-    callback(err);
-  } else {
-      db.account.find({username: username}, function(err, user){
-      if (!(user.length==0)) {
-        err = 'The username you entered already exists';
-        callback(err);
-      } else {
-            db.account.insert({username: username, password: password},function(err,user){
-            callback(err,user);
-        });
-      }
-    });
-  }
-}
-
-function authenticateUser(username, password, callback){
-
-  db.account.find({username: username, password: password}, function(err, user){
-    if ((user.length==0)||(user.password==0)) {
-      err = 'Credentials do not match';
-      callback(err);
-    } else {
-        callback(err, user);
-    }
-  });
-}
-
-// This is the client home page
 app.get('/',function(req, res) {
-    //res.render('login');
     res.sendFile(__dirname + '/client/login.html');
 });
 app.get('/about',function(req, res) {
-    //res.render('about');
     res.sendFile(__dirname + '/client/about.html');
 });
 app.get('/signup',function(req, res) {
-    //res.render('signup');
     res.sendFile(__dirname + '/client/signup.html');
 });
 app.get('/gamearea',function(req, res) {
-    //res.render('gamearea');
     res.sendFile(__dirname + '/client/gamearea.html');
 });
 app.get('/login',function(req, res) {
-    //res.render('login');
     res.sendFile(__dirname + '/client/login.html');
 });
 app.get('/error',function(req, res) {
-    //res.render('login');
     res.sendFile(__dirname + '/client/error.html');
 });
-//app.get('/game',function(req, res) {
-//    res.sendFile(__dirname + '/client/grid.html');
-//});
 
 app.post('/signupX', function(req, res){
   var username = req.body.username;
@@ -91,6 +40,7 @@ app.post('/signupX', function(req, res){
     }
   });
 });
+
 app.post('/loginX', function(req, res){
   var username = req.body.username;
   var password = req.body.password;
@@ -106,7 +56,6 @@ app.post('/loginX', function(req, res){
 });
 
 app.post('/move',function(req,res){
-  //console.log("received "+ req.params.rows + req.params.cols);
   var row = req.body.rows;
   var col = req.body.cols;
   console.log("received "+ row);
@@ -150,24 +99,24 @@ io.sockets.on('connection', function(socket){
     socket.on('hand', function(hand){
       var x = parseInt(hand[0]), y = parseInt(hand[1]);
       var conds = {
-	turn        : game.turn === player,
-	emptySquare : othello.getOccupant(game.board, x, y) === 0,
-	reversible  : othello.handReverseNum(game.board, game.turn, x, y) > 0
+      	turn        : game.turn === player,
+      	emptySquare : othello.getOccupant(game.board, x, y) === 0,
+      	reversible  : othello.handReverseNum(game.board, game.turn, x, y) > 0
       }
       if( conds.turn && conds.emptySquare && conds.reversible){
-	game.board = othello.hand(game.board, game.turn, x, y);
-	game.turn  = othello.nextPlayer(game.board, game.turn);
-	postBoard(game);
-	if( game.turn === 0 )
-	  gameEnd(game, othello.winner(game.board));
+      	game.board = othello.hand(game.board, game.turn, x, y);
+      	game.turn  = othello.nextPlayer(game.board, game.turn);
+      	postBoard(game);
+      	if( game.turn === 0 )
+      	  gameEnd(game, othello.winner(game.board));
       }
     });*/
 
     socket.on('disconnect', function(){
       if(GAME.players[0] === socket)
-	initGame();
+	       initGame();
       else
-	gameEnd(game, othello.anotherPlayer(player));
+	       //gameEnd(game, othello.anotherPlayer(player));
     });
 
     if(game.players.length === 2){
@@ -187,7 +136,6 @@ function handler (req, res) {
 function initGame(){
   GAME = {
     players: [],
-    // add the code to inital array
     board:   [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0] ,[0,0,0,1,2,0,0,0], [0,0,0,2,1,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0] ],
     turn:    0
   }
@@ -205,6 +153,35 @@ function gameEnd(game, winner){
 function postBoard(game){
   game.players.map(function(player){
     player.emit('board', {board: game.board, turn: game.turn});
+  });
+}
+
+function createUser(username, password, password_confirmation, callback){
+    if (password !== password_confirmation) {
+    var err = 'The passwords do not match';
+    callback(err);
+  } else {
+      db.account.find({username: username}, function(err, user){
+      if (!(user.length==0)) {
+        err = 'The username you entered already exists';
+        callback(err);
+      } else {
+            db.account.insert({username: username, password: password},function(err,user){
+            callback(err,user);
+        });
+      }
+    });
+  }
+}
+
+function authenticateUser(username, password, callback){
+  db.account.find({username: username, password: password}, function(err, user){
+    if ((user.length==0)||(user.password==0)) {
+      err = 'Credentials do not match';
+      callback(err);
+    } else {
+        callback(err, user);
+    }
   });
 }
 
